20f53b8e8f7d4c7b52f67569b1a893def52a1738
Manik Maity <manikmaity156@gmail.com>
2024-09-30 08:55:27 +0530

    Created pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Sigin from "./pages/Sigin";
+import Signup from "./pages/Signup";
+
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/sign-in" element={<Sigin/>} />
+        <Route path="/Sign-up" element={<Signup/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
