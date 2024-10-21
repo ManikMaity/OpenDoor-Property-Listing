f08af0e7acbc6037a207c465b58fd0ea70a52930
Manik Maity <manikmaity156@gmail.com>
2024-10-21 20:32:18 +0530

    Started user update

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,12 @@
 export function test (req, res) {
     res.json({msg : "API route is working!"});
 }
+
+export async function update(req, res, next) {
+    try{
+
+    }
+    catch(err){
+        next(err);
+    }
+}
--- api/route/user.route.js
+++ api/route/user.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { test } from "../controllers/user.controller.js";
+import { test, update } from "../controllers/user.controller.js";
 const userRouter = express.Router();
 
 userRouter.get("/", test);
+userRouter.post("/update/:id", update);
 
 export default userRouter;
